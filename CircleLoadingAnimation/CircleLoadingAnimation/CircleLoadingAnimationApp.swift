201cf0980e9e10260ac4642a079f79181f6cd9e8
shirangabriel <gabriel.stcb@gmail.com>
2024-11-08 11:38:24 +0530

    initial commit

---
+++ CircleLoadingAnimation/CircleLoadingAnimation/CircleLoadingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleLoadingAnimationApp.swift
+//  CircleLoadingAnimation
+//
+//  Created by Shiran Gabriel on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct CircleLoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
