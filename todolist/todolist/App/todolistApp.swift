a87f6ca8d32d995e69499e38df7579510f615bc5
Shiran Gabriel <gabriel.stcb@gmail.com>
2025-01-25 23:50:36 +0530

    enbale core data

---
+++ todolist/todolist/App/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Gabe on 2025-01-14.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
