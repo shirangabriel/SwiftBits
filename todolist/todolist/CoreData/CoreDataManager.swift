e5ea6a13e314e6476480ffb01513eab3fe64d243
Shiran Gabriel <gabriel.stcb@gmail.com>
2025-01-25 23:51:09 +0530

    enable core data

---
+++ todolist/todolist/CoreData/CoreDataManager.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataManager.swift
+//  todolist
+//
+//  Created by Gabe on 2025-01-25.
+//
+
+import CoreData
+
+class CoreDataManager {
+    static let coreDataManager  = CoreDataManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "todolist")
+        
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Unresolved error \(error)")
+            }
+        }
+        
+        return container
+    }()
+    
+    
+    var context: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}
